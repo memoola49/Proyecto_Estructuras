059539b79f91dd522f2d9ee5085d7df5fdeed3c5
memoola49 <55213964+memoola49@users.noreply.github.com>
2019-11-27 13:35:25 -0600

    Update agenda.h

--- agenda.h
+++ agenda.h
@@ -5,16 +5,23 @@ struct nodo{
 	struct persona *p;
 	struct nodo *sig;
 }nodo;
+
 struct persona{
 char *nombre;
 long telefono;
 char *direccion;
 int fav;
 
-};
+}persona;
+
 struct nodo* crearlista(struct nodo *lista);
 struct nodo* agregar(struct nodo *lista,struct persona *p);
 void mostrarlista(struct nodo *lista);
-struct nodo *eliminar(struct nodo *lista,char *nombre);
+struct nodo *eliminar(struct nodo *lista,char *n);
+struct nodo *ordenalf(struct nodo *lista);
+struct nodo *ordenfav(struct nodo *lista);
+struct nodo *ordenarec(struct nodo *lista);
+
+
 
 #endif
